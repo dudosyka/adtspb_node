0ca6c7958982053690acd0c363e217ce8289ba85
dudosyka <a.hatson22@gmail.com>
2021-03-13 00:30:49 +0300

    Token validation. Routing (base). Authorization (base).
    Co-Authored-By: Санёк <dudosyka@users.noreply.github.com>

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -1,15 +1,26 @@
 <template>
   <div class="home">
-    <h2>Main TEST</h2>
-    <img alt="Vue logo" src="../assets/logo.png">
+    <h1>Home Page</h1>
+    <button onclick='localStorage.removeItem("token");window.location = window.location;'>LOGOUT</button>
   </div>
 </template>
 
 <script>
+// @ is an alias to /src
 
 export default {
   name: 'Home',
   components: {
+  },
+  data() {
+      return {
+      }
+  },
+  methods: {
+  },
+  async created()
+  {
+      this.token = localStorage.getItem('token');
   }
 }
 </script>
---
+++ vue/src/views/Page2.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="page2">
+    <h1>Page2 Page</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Page2',
+  components: {
+  },
+  data() {
+      return {
+      }
+  },
+  methods: {
+  },
+  async created()
+  {
+  }
+}
+</script>
