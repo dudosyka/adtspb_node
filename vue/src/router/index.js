27a7bf6d5a24822365e477eb4aaed451a25b482c
dudosyka <a.hatson22@gmail.com>
2021-03-14 14:35:11 +0300

    Token validation. Routing (base). Authorization (base).
    Co-Authored-By: Санёк <dudosyka@users.noreply.github.com>

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -1,5 +1,10 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import Vue from 'vue'
+import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Login from '../views/Login.vue'
+import Page2 from '../views/Page2.vue'
+
+Vue.use(VueRouter)
 
 const routes = [
   {
@@ -8,18 +13,20 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/login',
+    name: 'Authorization',
+    component: Login
+  },
+  {
+    path: '/page',
+    name: 'Page2',
+    component: Page2
   }
 ]
 
-const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+const router = new VueRouter({
+  mode: 'history',
   routes
-})
+});
 
 export default router
