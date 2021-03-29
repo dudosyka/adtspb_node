0f5fdb3edb86536b0790240a516aee414c7d53e4
dudosyka <a.hatson22@gmail.com>
2021-03-29 17:06:26 +0300

    1. Structure updates.CSS import in App. Layouts. SignUp in router, sign up view file. 2. Edit manual 3. New styles for checkboxes. 4. Npm updates.
    Co-Authored-By: Санёк <dudosyka@users.noreply.github.com>
    Co-Authored-By: Mihail <MohonovProduction@users.noreply.github.com>

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,22 +1,20 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> | 
-      <router-link to="/page">Page</router-link>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/page">Page</router-link> |
+      <router-link to="/signup">SignUp</router-link>
     </div>
-    <router-view/>
+
+    <component :is="layout">
+        <router-view />
+    </component>
   </div>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
+@import "./assets/styles/style.css";
+/*  hj */
 #nav {
   padding: 30px;
 }
@@ -24,9 +22,25 @@
 #nav a {
   font-weight: bold;
   color: #2c3e50;
-}
+}s
 
 #nav a.router-link-exact-active {
   color: #42b983;
 }
 </style>
+
+<script type="text/javascript">
+  import AuthLayout from '@/layouts/AuthLayout'
+  import HomeLayout from '@/layouts/HomeLayout'
+
+  export default {
+    computed: {
+      layout() {
+        return (this.$route.meta.layout || 'AuthLayout' )
+      }
+    },
+    components: {
+      AuthLayout, HomeLayout
+    }
+  }
+</script>
--- vue/src/views/Login.vue
+++ vue/src/views/Login.vue
@@ -1,14 +1,6 @@
 <template>
-  <div class="login">
-
-    <a class="main-link">
-      <div class="row-left"></div>
-      <span>Академия цифровых технологий</span>
-    </a>
-
-    <main class="main-content">
+  <main class="main-content">
       <div class="auth">
-
           <div class="input-container">
             <label class="label" v-bind:class="{'label-up': login}">номер телефона/электронная почта</label><br>
             <input type="text" v-model="login" class="type" tabindex="1">
@@ -23,33 +15,36 @@
 
           <div class="checkbox-container" v-on:click="remember = !remember">
             <input type="checkbox" v-model="remember" class="checkbox" tabindex="3">
-            <label class="air-dark-button">Запомнить меня</label>
+            <label class="air-dark-button checkbox">Запомнить меня</label>
           </div>
 
           <div class="buttons">
             <button class="dark-button" @click="auth()" tabindex="4">Войти</button>
-            <button class="light-button" tabindex="5">Регистрация</button>
+            <router-link to="/signup" tabindex="5" class="light-button">Регистрация</router-link>
           </div>
       </div>
 
       <div class="plate">
         <h1 class="title">Добро<br> пожаловать<br> в личный кабинет</h1>
-        <!--
-        <aside class="social-media">
-          <a href="#" class="link-icon link-tl"></a>
-          <a href="#" class="link-icon link-vk"></a>
-          <a href="#" class="link-icon link-fb"></a>
-          <a href="#" class="link-icon link-in"></a>
+        <aside class="social-media-list container">
+          <ul class="social-media-list ul">
+              <li class="social-media-list li">
+                <a href="https://t.me/adtspb" class="fab fa-telegram-plane social-media-link" target="_blank"></a>
+              </li>
+              <li class="social-media-list li">
+                <a href="https://vk.com/adtspb" class="fab fa-vk social-media-link" target="_blank"></a>
+              </li>
+              <li class="social-media-list li">
+                <a href="https://www.facebook.com/adtspb" class="fab fa-facebook-square social-media-link" target="_blank"></a>
+              </li>
+              <li class="social-media-list li">
+                <a href="https://www.instagram.com/adtspb" class="fab fa-instagram social-media-link" target="_blank"></a>
+              </li>
+          </ul>
         </aside>
-        !-->
+
       </div>
-    </main>
-    <!--
-    <input type="text" v-model="login" placeholder="Email or phone">
-    <input type="pass" v-model="pass" placeholder="pass">
-    <button @click="auth()">Token</button>
-    !-->
-  </div>
+  </main>
 </template>
 <script>
   import axios from "axios"
@@ -62,6 +57,9 @@
         pass: null,
         remember: false //not is parm
       }
+    },
+    components: {
+
     },
     methods: {
       auth()
