0f868f0dfd00ef0f9f03be80d2c4f5d7800573ad
dudosyka <45633581+dudosyka@users.noreply.github.com>
2021-05-13 23:16:17 +0300

    Add port to AppConfig

---
+++ graphql/config/AppConfig.js
@@ -0,0 +1,9 @@
+module.exports = {
+    //Change to valid when set on production
+    parent_role_id: 8,
+    child_role_id: 10,
+    common_user_id: 9,
+    port: 8080
+}
+
+//id: 6 - "password": "tryeuie",
