00e0dc92661bd25259e4ac7e5455ed5127f6a1d5
dudosyka <45633581+dudosyka@users.noreply.github.com>
2021-05-13 23:18:21 +0300

    Merge patch_back into master

--- graphql/config/AppConfig.js
+++ graphql/config/AppConfig.js
@@ -3,10 +3,7 @@ module.exports = {
     parent_role_id: 8,
     child_role_id: 10,
     common_user_id: 9,
-<<<<<<< HEAD
-=======
     port: 8080
->>>>>>> patch_back
 }
 
 //id: 6 - "password": "tryeuie",
