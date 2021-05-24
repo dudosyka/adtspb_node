9c360d02a7b2ea999c02bb599f73ef3611dfcc56
dudosyka <45633581+dudosyka@users.noreply.github.com>
2021-05-24 12:03:06 +0300

    Email validation token fixes.
    Co-Authored-By: Alex <dudosyka@users.noreply.github.com>

--- graphql/Entity/EmailValidation.js
+++ graphql/Entity/EmailValidation.js
@@ -52,7 +52,7 @@ EmailValidation.prototype.generateToken = function (l) {
     let res = "";
     let getRandomSymbol = () => {
         const symbols = "a,b,c,d,e,f,g,h,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,A,B,C,D,E,F,G,H,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z,1,2,3,4,5,6,7,8,9".split(",");
-        return symbols[Math.floor(0 + Math.random() * (symbols.length + 1 - 0))];
+        return symbols[Math.floor(Math.random() * (symbols.length - 1))];
     }
     for (let i = 0; i < l; i++) {
         let symbol = getRandomSymbol();
