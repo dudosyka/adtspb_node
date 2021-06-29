4369f6c1526d0d572457408c9bca629cdccad038
dudosyka <45633581+dudosyka@users.noreply.github.com>
2021-06-29 17:19:12 +0300

    Mail config

--- graphql/utils/Mail.js
+++ graphql/utils/Mail.js
@@ -4,10 +4,14 @@ let Mail = function () {}
 
 Mail.prototype.sendEmail = async function (to, subject, text) {
     let transporter = nodemailer.createTransport({
-      host: "localhost"
+      service: "Yandex",
+      auth: {
+          user: "lk_support@adtspb.ru",
+          pass: "ZYxu8*#sxZSs"
+      }
     });
     await transporter.sendMail({
-      from: '"Node js" <nodejs@example.com>',
+      from: '"Node js" <lk_support@adtspb.ru>',
       to: to,
       subject: subject,
       text: text,
