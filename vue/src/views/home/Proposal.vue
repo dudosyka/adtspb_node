72d65e1f66fadfeab6272ff694f495fa9c20f336
Mihail <mohonovproduction@yandex.ru>
2021-07-14 16:08:07 +0300

    warn container in header

--- vue/src/views/home/Proposal.vue
+++ vue/src/views/home/Proposal.vue
@@ -3,6 +3,23 @@
         <navigation />
 
         <section class="home-content">
+            <section class="warning-container">
+                <p>Функционал станет доступнен в даты подачи заявлений. Объявление будет размещено заранее, следите за информацией в официальных сообщества Академии Цифровых Технологий и <a href="https://adtspb.ru" target="_blank" class="link_text">на сайте</a>.</p>
+                <div class="link-container">
+                    <div class="social-media-list li">
+                        <a href="https://t.me/adtspb" class="fab fa-telegram-plane social-media-link" target="_blank"></a>
+                    </div>
+                    <div class="social-media-list li">
+                        <a href="https://vk.com/adtspb" class="fab fa-vk social-media-link" target="_blank"></a>
+                    </div>
+                    <div class="social-media-list li">
+                        <a href="https://www.facebook.com/adtspb" class="fab fa-facebook-square social-media-link" target="_blank"></a>
+                    </div>
+                    <div class="social-media-list li">
+                        <a href="https://www.instagram.com/adtspb" class="fab fa-instagram social-media-link" target="_blank"></a>
+                    </div>
+                </div>
+            </section>
 
             <article class="card shadow children" v-for="child in children">
                 <h2 class="child-name">{{ child.name + ' ' + child.surname }}</h2>
@@ -16,7 +33,7 @@
                             <figcaption class="child-stat_heading">Статус: {{ proposal.status.text }}</figcaption>
                         </div>
                         <div class="buttons" v-if='proposal.status.num !== 0'>
-                            <p class="warning-container">Кнопки станут активны перед началом очного приёма документов</p>
+                        
                             <button class="dark-button wp100" @click="downloadPdf(proposal.id, child, index)" disabled>Скачать</button>
                             <button class="dark-button wp100" @click="printPdf(proposal.id)" disabled>Печатать</button>
 
@@ -53,7 +70,19 @@
     }
 }
 .warning-container {
+    padding: 20px;
+    margin-bottom: 30px;
+    max-width: 600px;
+    min-width: 300px;
+    box-sizing: border-box;
+}
+.link-container {
     padding: 10px;
+    height: 35px;
+    display: flex;
+    justify-content: space-around;
+    background-color: #615d39;
+    border-radius: 30px;
 }
 .buttons {
     max-width: 200px;
