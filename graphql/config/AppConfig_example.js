3fc1fb6d2b4441c58d01e4998fef7ca42f14c2ae
dudosyka <45633581+dudosyka@users.noreply.github.com>
2021-07-21 22:19:27 +0300

    Admin role checking in backend

--- graphql/config/AppConfig_example.js
+++ graphql/config/AppConfig_example.js
@@ -1,8 +1,9 @@
 //To app work correct add new AppConfig.js file into this dir. It content should be like this: (If you need then change it compared with your data)
 module.exports = {
     parent_role_id: 8, //Id of `parent` role in your `role` table
-    child_role_id: 10, //Id of `child` role in your `role` table
     common_user_id: 9, //Id of `simmple user` role in your `role` table
+    child_role_id: 10, //Id of `child` role in your `role` table
+    admin_role_id: 11, //Id of `admin` role in your `role` table
     port: 8080,
     year: 2021,
     group_size: 15,
--- graphql/types/Admin/Query.js
+++ graphql/types/Admin/Query.js
@@ -2,6 +2,8 @@ const graphql = require('graphql');
 const Admin = require('../../Entity/Admin');
 const admin = new Admin();
 
+const User = require('../../Entity/User');
+
 module.exports = new graphql.GraphQLObjectType({
     name: "AdminQuery",
     fields: () => ({
@@ -9,6 +11,9 @@ module.exports = new graphql.GraphQLObjectType({
             type: StatOutput,
             args: {},
             async resolve(obj) {
+                const user = await User.createFrom({id: obj.viewer.id}, false, false);
+                if (!user.hasAccess(15))
+                    throw Error('Forbidden');
                 return await admin.getStat();
             }
         }
