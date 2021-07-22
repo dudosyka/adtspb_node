86a22386a1b6b77d333db7a8dfc2415b7cba7e36
Mihail <mohonovproduction@yandex.ru>
2021-07-22 10:58:03 +0300

    front config fix

---
+++ vue/src/config/AppConfig.js
@@ -0,0 +1,11 @@
+module.exports = {
+    api_url: "http://localhost:8080/api",
+    endoor_url: "http://localhost:8080/endoor",
+    parent_role_id: 8,
+    common_user_id: 9,
+    child_role_id: 10,
+    admin_role_id: 11,
+    max_hours_week: 16,
+    min_hours_week: 12,
+    inDev: true,
+}
