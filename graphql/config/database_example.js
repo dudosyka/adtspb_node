f329f7b5b0b2fc0a032b3074ba3a4bc8dbbf80b0
Mihail <mohonovproduction@yandex.ru>
2021-08-10 23:12:29 +0300

    db hot fix

--- graphql/config/database_example.js
+++ graphql/config/database_example.js
@@ -1,7 +1,7 @@
 //To use db connection add new database.js file into this dir. It content should be like this:
 module.exports = {
     host: "localhost",
-    database: "adt",
-    user: "root",
-    password: "",
+    database: "adtspb_node",
+    user: "user",
+    password: "123",
 }
