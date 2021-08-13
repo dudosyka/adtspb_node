d1de271e9c7ec53c8712a9d6a4c215a8a9c5d62f
Mihail <mohonovproduction@yandex.ru>
2021-08-14 01:25:12 +0300

    association timetable and description

--- admin/src/views/Associations.vue
+++ admin/src/views/Associations.vue
@@ -11,8 +11,39 @@
 			<article class="card bg-card">
 				<button @click="show.association = false" class="bg-card ft-gray button-close">Х</button>
 				<h2 class="card-title" v-text="association.name"></h2>
-				<button class="ft-dark">Редактировать описание</button>
-				<p class="card-text" v-text="association.description"></p>
+				
+				<section class="container assoc-description">
+					<h3>Описание</h3>
+					<textarea 
+						v-model="association.description" 
+						cols="50" rows="5"
+						placeholder="Описание объединения" 
+					></textarea>
+					<button class="ft-dark">Редактировать описание</button>
+				</section>
+
+				<section class="container">
+					<h3>Рассписание групп</h3>
+					<article class="groups-container">
+						<ul class="group-list">
+							<li class="group-list_item"
+								:class="{'group-list_item--active': group.show}" 
+								v-for="group of association.groups" 
+								v-text="group.name" 
+								@click="association.groups.map(el => el.show = false); group.show = true"
+							></li>
+						</ul>
+						<ul class="group-data-list">
+							<li v-for="group of association.groups">
+								<table>
+									<tr v-for="(time, day) of group.timetable" v-if="group.show">
+										<td v-text="day"></td><td v-text="time"></td>
+									</tr>
+								</table>
+							</li>
+						</ul>
+					</article>
+				</section>
 			</article>
 		</div>
 	</main>
@@ -21,6 +52,9 @@
 <style scoped>
 main {
 	min-height: 100vh;
+}
+.card-wrapper {
+	padding: 30px 30px 0 30px;
 }
 .assoc-list {
 	display: flex;
@@ -47,9 +81,34 @@ main {
 .assoc-title {
 	white-space: nowrap;
 }
-.assoc-descriptoin {
-
+.assoc-description textarea {
+	margin-bottom: 5px;
 }
+.groups-container {
+	display: grid;
+	grid-template-columns: auto 1fr;
+}
+.group-list, .group-data-list {
+	list-style: none;
+	padding: 0;
+	margin: 0;
+}
+.group-list_item {
+	padding: 5px;
+}
+.group-list_item:hover {
+	cursor: pointer;
+}
+.group-list_item--active {
+	text-decoration: underline;
+}
+.group-data-list td {
+	padding: 5px 10px;
+}
+.container h3 {
+	margin-bottom: 5px;
+}
+ 
 </style>
 
 <script>
@@ -67,7 +126,29 @@ export default {
             associations: [
             	{
             		name: 'Test association',
-            		description: 'Bla bla bla',
+            		description: 'Bla bla bla slkfh skafhsa sakfhsadkl safkjhsakfl sadkkfhsadkf sadkfhskdf fsakdfh sakdfhsdks skafhs skkafh ksahfsa  sakdfh safk saksdahfskdfhs fksadfh skadfhskd sdkfhsad sdjfhsdka sadkfjhsda',
+            		groups: [
+            			{	
+            				show: true,
+            				name: 'group 1',
+            				timetable: {
+            					'понедельник': '17:00 - 18:00',
+            					'суббота': '17:00 - 18:00',
+            					'четверг': '17:00 - 18:00',
+            					'пятница': '17:00 - 18:00',
+            				}
+            			},
+            			{	
+            				show: false,
+            				name: 'group 2',
+            				timetable: {
+            					'понедельник': '17:00 - 18:00',
+            					'суббота': '17:00 - 18:00',
+            					'четверг': '17:00 - 18:00',
+            					'пятница': '17:00 - 18:00',
+            				}
+            			}
+            		]
             	},
             	{
             		name: 'Test association',
