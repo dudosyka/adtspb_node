110be75474c7ce4fc45f7e370e76cfbba05c68ee
dudosyka <45633581+dudosyka@users.noreply.github.com>
2021-08-14 21:16:33 +0300

    AccessControl fix. (Output roles w\o rules)

--- graphql/Entity/AccessControl.js
+++ graphql/Entity/AccessControl.js
@@ -8,8 +8,9 @@ const rbac = new Rbac();
 let AccessControl = function () {}
 
 AccessControl.prototype.getRights = async function (user_id) {
-    const rights = await db.query('SELECT `main`.`user_id`, `assignment`.`role`, `assignment`.`rule`, `role`.`name` as `role_name`, `role`.`description` as `role_description`, `rule`.`name` as `rule_name`, `rule`.`description` as `rule_description`  FROM `user_role` as `main` LEFT JOIN `auth_role` as `role` ON `main`.`auth_role_id` = `role`.`id` LEFT JOIN `auth_assignment_min` as `assignment` ON `main`.`auth_role_id` = `assignment`.`role` LEFT JOIN `auth_rule` as `rule` ON `assignment`.`rule` = `rule`.`id` WHERE `user_id` = ?', [ user_id ]);
+    const rights = await db.query('SELECT `main`.`user_id`, `main`.`auth_role_id` as `role`, `assignment`.`rule`, `role`.`name` as `role_name`, `role`.`description` as `role_description`, `rule`.`name` as `rule_name`, `rule`.`description` as `rule_description`  FROM `user_role` as `main` LEFT JOIN `auth_role` as `role` ON `main`.`auth_role_id` = `role`.`id` LEFT JOIN `auth_assignment_min` as `assignment` ON `main`.`auth_role_id` = `assignment`.`role` LEFT JOIN `auth_rule` as `rule` ON `assignment`.`rule` = `rule`.`id` WHERE `user_id` = ?', [ user_id ]);
     const roles = {};
+    console.log(rights);
     rights.map(right => {
         if (roles[right.role]) {
             roles[right.role].rules.push({
@@ -23,7 +24,7 @@ AccessControl.prototype.getRights = async function (user_id) {
             id: right.role,
             name: right.role_name,
             description: right.role_description,
-            rules: [
+            rules: right.rule == null ? [] : [
                 {
                     id: right.rule,
                     name: right.rule_name,
@@ -32,6 +33,7 @@ AccessControl.prototype.getRights = async function (user_id) {
             ]
         };
     });
+    console.log(roles);
     const output = [];
     Object.keys(roles).map(el => {
         output.push(roles[el]);
