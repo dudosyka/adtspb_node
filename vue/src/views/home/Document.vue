f0b8564b84d2bca96b7720b9544450158629f9c7
Mihail <mohonovproduction@yandex.ru>
2021-08-20 20:32:00 +0300

    Marinas widget is work

--- vue/src/views/home/Document.vue
+++ vue/src/views/home/Document.vue
@@ -3,7 +3,10 @@
         <navigation />
 
         <article class="home-content">
-            
+            <div class="card shadow">
+                <h1>Если не произошло автоматическое направление, нажмите на кнопку ниже</h1>
+                <a class="dark-button" href="https://widget.easyweek.io/act/">Открыть</a>
+            </div>
         </article>
     </main>
 </template>
@@ -27,6 +30,11 @@
     background-color: #615d39;
     border-radius: 30px;
 }  
+.card {
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-gap: 10px;
+}
 </style>
 
 <script>
@@ -36,6 +44,9 @@
     name: '',
     components: {
       navigation,
+    },
+    mounted() {
+        window.open('https://widget.easyweek.io/act/', '_blank')
     },
     data() {
       return {
