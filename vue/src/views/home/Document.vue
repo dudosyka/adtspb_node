3046a58e0a68ff61c660b32370aaa3ade9fbc641
Mihail <mohonovproduction@yandex.ru>
2021-08-20 17:10:46 +0300

    remove atention baner

--- vue/src/views/home/Document.vue
+++ vue/src/views/home/Document.vue
@@ -3,23 +3,7 @@
       <navigation />
 
       <article class="home-content">
-        <section class="warning-container shadow">
-                <p>Функционал станет доступнен в даты подачи заявлений. Объявление будет размещено заранее, следите за информацией в официальных сообщества Академии Цифровых Технологий и <a href="https://adtspb.ru" target="_blank" class="link_text">на сайте</a>.</p>
-                <div class="link-container">
-                    <div class="social-media-list li">
-                        <a href="https://t.me/adtspb" class="fab fa-telegram-plane social-media-link" target="_blank"></a>
-                    </div>
-                    <div class="social-media-list li">
-                        <a href="https://vk.com/adtspb" class="fab fa-vk social-media-link" target="_blank"></a>
-                    </div>
-                    <div class="social-media-list li">
-                        <a href="https://www.facebook.com/adtspb" class="fab fa-facebook-square social-media-link" target="_blank"></a>
-                    </div>
-                    <div class="social-media-list li">
-                        <a href="https://www.instagram.com/adtspb" class="fab fa-instagram social-media-link" target="_blank"></a>
-                    </div>
-                </div>
-            </section>
+
       </article>
   </main>
 </template>
