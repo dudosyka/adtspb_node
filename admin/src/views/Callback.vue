39ed2dfa00740b1d39c922d9f7e2ec7583c7bb85
Mihail <mohonovproduction@yandex.ru>
2021-08-24 17:15:18 +0300

    edit data render

--- admin/src/views/Callback.vue
+++ admin/src/views/Callback.vue
@@ -8,127 +8,50 @@
                         <b-card title="Поиск запросам на редактирование данных">
                         <b-card-body>
                             <b-input-group prepend='Фамилия'>
-                                <b-input v-model="searchString"/>
+                                <b-input v-on:keyup.enter="search" v-model="searchString"/>
                             </b-input-group>
                         </b-card-body>
                         <b-button @click="search" variant="primary" block size="lg" style="width: 100%">Найти</b-button>     
                         </b-card>
                         <b-overlay :show="overlay">
-                            <!--
-                            <b-card v-if="searchStep" title="Результаты поиска">
+                            <b-card v-for="request of requestsOnEdit">
                                 <b-container>
-                                    <b-card>
-                                        <b-card-text>
-                                            Данные
-                                        </b-card-text>
-                                        <b-card-body>
-                                            <b-input-group prepend="Фамилия">
-                                                <b-input  v-model='target' />
-                                            </b-input-group>
-                                            <b-input-group prepend="Имя">
-                                                <b-input v-model='proposal.child.name' />
-                                            </b-input-group>
-                                            <b-input-group prepend="Отчество">
-                                                <b-input v-model='proposal.child.lastname' />
-                                            </b-input-group>
-                                        </b-card-body>
-                                        <b-card-body>
-                                            <b-input-group prepend="Дата рождения">
-                                                <b-form-datepicker placeholder="" v-model="proposal.child.birthday" />
-                                            </b-input-group>
-                                        </b-card-body>
-                                        <b-card-body>
-                                            <b-input-group  prepend="Почта">
-                                                <b-input v-model='proposal.child.email' />
-                                            </b-input-group>
-                                            <b-input-group  prepend="Номер телефона">
-                                                <b-input v-model='proposal.child.phone' />
-                                            </b-input-group>
-                                        </b-card-body>
-                                        <b-card-body>
-                                            Пол
-                                            <b-form-radio :name="`sexChild${proposal.id}`" v-model='proposal.child.sex' :value="1">Мужской</b-form-radio>
-                                            <b-form-radio :name="`sexChild${proposal.id}`" v-model='proposal.child.sex' :value="0">Женский</b-form-radio>
-                                        </b-card-body>
-                                        <b-card-body>
-                                            <b-form-checkbox
-                                                :value="1"
-                                                :unchecked-value="0"
-                                                 v-model='proposal.child.ovz'
-                                            >
-                                                ОВЗ
-                                            </b-form-checkbox>
-                                            Тип ОВЗ<b-form-select :options="ovz_types" v-model="proposal.child.ovz_type.id" />
-                                        </b-card-body>
-                                        <b-card-body>
-                                            <b-form-checkbox
-                                                :value="1"
-                                                :unchecked-value="0"
-                                                v-model='proposal.child.disability'
-                                            >
-                                                Инвалидность
-                                            </b-form-checkbox>
-                                            Группа инвалидности<b-form-select :options="disability_types" v-model="proposal.child.disability_group.id"/>
-                                        </b-card-body>
-                                        <b-card-body>
-                                            <b-input-group prepend="Учебное заведение (наименование)">
-                                                <b-input v-model='proposal.child.studyPlace' />
-                                            </b-input-group>
-                                        </b-card-body>
-                                        <b-card-body>
-                                            <b-input-group  prepend="Гражданство">
-                                                <b-input v-model='proposal.child.state' />
-                                            </b-input-group>
-                                        </b-card-body>
-                                        <b-card-body>
-                                            <b-input-group prepend="Степень родства">
-                                                <b-input v-model="proposal.child.relationship" />
-                                                <b-button @click="proposal.child.relationship = 'Законный представитель'">Законный представитель</b-button>
-                                            </b-input-group>
-                                        </b-card-body>
-                                        <b-card-body>
-                                            Адрес регистрации
-                                            <b-input-group prepend="Город">
-                                                <b-input v-model='proposal.child.registration_address.city' />
-                                            </b-input-group>
-                                            <b-input-group prepend="Район">
-                                                <b-input v-model='proposal.child.registration_address.district' />
-                                            </b-input-group>
-                                            <b-input-group prepend="Улица">
-                                                <b-input v-model='proposal.child.registration_address.street' />
-                                            </b-input-group>
-                                            <b-input-group prepend="Дом">
-                                                <b-input v-model='proposal.child.registration_address.house' />
-                                            </b-input-group>
-                                            <b-input-group prepend="Квартира">
-                                                <b-input v-model='proposal.child.registration_flat' />
-                                            </b-input-group>
-                                        </b-card-body>
-                                        <b-card-body>
-                                            Адрес проживания
-                                            <b-input-group prepend="Город">
-                                                <b-input v-model='proposal.child.residence_address.city' />
-                                            </b-input-group>
-                                            <b-input-group prepend="Район">
-                                                <b-input v-model='proposal.child.residence_address.district' />
-                                            </b-input-group>
-                                            <b-input-group prepend="Улица">
-                                                <b-input v-model='proposal.child.residence_address.street' />
-                                            </b-input-group>
-                                            <b-input-group prepend="Дом">
-                                                <b-input v-model='proposal.child.residence_address.house' />
-                                            </b-input-group>
-                                            <b-input-group prepend="Квартира">
-                                                <b-input v-model='proposal.child.residence_flat' />
-                                            </b-input-group>
-                                        </b-card-body>
-                                    </b-card>
-                                    <b-card>
-                                        
-                                    </b-card>
+                                    <b-row>
+                                        <b-card 
+                                            class="col-sm"
+                                            :title="`кто ${request.requester.surname} ${request.requester.name} ${request.requester.lastname}`"
+                                        />
+                                        <b-card
+                                            class="col-sm" 
+                                            :title="`кому ${request.requester.surname} ${request.requester.name} ${request.requester.lastname}`"
+                                        />
+                                    </b-row>
+                                    <b-row>
+                                        <b-card 
+                                            class="col-sm"
+                                            title="Старые данные"
+                                        >
+                                            <b-card-text
+                                                v-text="request.old_value"
+                                            />
+                                        </b-card>
+                                        <b-card 
+                                            class="col-sm"
+                                            title="Новые данные"
+                                        >
+                                            <b-card-text
+                                                v-text="request.new_value"
+                                            />
+                                        </b-card>
+                                    </b-row>
+                                    <b-card-body>
+                                        <b-row>
+                                            <b-button class="col-sm" variant="success" @click="acceptEditData(request)">Одобрить</b-button>
+                                            <b-button class="col-sm" variant="danger" @click="cancelEditData(request)">Отклонить</b-button>
+                                        </b-row>
+                                    </b-card-body>
                                 </b-container>
                             </b-card>
-                            !-->
                         </b-overlay>
                     </b-tab>
                     <b-tab title="Удаление">
@@ -138,27 +61,6 @@
                     </b-tab>
                 </b-tabs>
             </div>
-            <!--
-            <b-card v-for="request of requests">
-                <b-button v-b-toggle="`${request.id}child`">{{request}}</b-button>
-                <b-collapse :id="`${request.id}child`">
-                    <b-card-body>
-                        <b-card-group>
-                            <b-card title="Parent name">
-                                
-                            </b-card>
-                            <b-card title="Child name">
-
-                            </b-card>
-                        </b-card-group>
-                    </b-card-body>
-                    <b-card-body>
-                        <b-button variant='success'>Сохранить</b-button>
-                        <b-button variant='danger'>Удалить</b-button>
-                    </b-card-body>
-                </b-collapse>
-            </b-card>
-            !-->
         </b-card-group>
 	</main>
 </template>
@@ -205,6 +107,12 @@ export default {
                     this.requestsOnDelet = data
                     this.overlay = false
                 } ) 
+        },
+        acceptEditData(request) {
+            this.overlay = true
+        },
+        cancelEditData(request) {
+            this.overlay = true
         }
     }
 }
