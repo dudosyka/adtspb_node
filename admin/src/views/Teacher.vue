074478871ab47814d9b7b6b5f4f73044ac8931b3
Mihail <mohonovproduction@yandex.ru>
2021-08-30 18:29:53 +0300

    sasha

--- admin/src/views/Teacher.vue
+++ admin/src/views/Teacher.vue
@@ -2,10 +2,17 @@
     <main class="bg-wrapper">
         <Header />
         <b-overlay :show="overlay">
-            <b-card>
-                <b-button></b-button>
-                <b-collapse>
+            <b-card v-for="association in associations">
+                <b-button v-b-toggle="`Association${association.id}`"></b-button>
+                <b-collapse :id="`Association${association.id}`">
+                    <b-tabs>
+                        <b-tab
+                            v-for="group in association.groups"
+                            :title="group.name"
+                        >
 
+                        </b-tab>
+                    </b-tabs>
                 </b-collapse>
             </b-card>
         </b-overlay>
@@ -23,8 +30,19 @@ export default {
     },
     data() {
         return {
-            overlay: false
+            overlay: false,
+            associations: [],
         }
+    },
+    created() {
+        this.overlay = true
+        Admin.getAssociations().then(data => {
+            this.associations = data
+            this.overlay = false
+        })
+    },
+    methods: {
+
     },
 }
 </script>
