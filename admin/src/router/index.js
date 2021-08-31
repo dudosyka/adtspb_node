16bc967dca4a2c92291f0c0c5ddf368b99fa06de
Mihail <mohonovproduction@yandex.ru>
2021-08-31 17:43:01 +0300

    teacher page

--- admin/src/router/index.js
+++ admin/src/router/index.js
@@ -31,7 +31,7 @@ const routes = [
   {
     path: '/for_teacher',
     name: 'Teacher',
-    component: () => import('../views/Proposals.vue')
+    component: () => import('../views/Teacher.vue')
   }
 ]
 
---
+++ admin/src/views/Teacher.vue
@@ -0,0 +1,25 @@
+<template>
+    <main class="bg-wrapper">
+        <Header />
+    </main>
+</template>
+
+<script>
+import Header from '../components/Header'
+import {Admin} from '../models/Admin'
+import {Parser} from '../utils/Parser'
+
+export default {
+    name: "Teacher",
+    components: {
+        Header
+    },
+    methods: {
+
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
