8925e2ab8d9a484da06b0505c5c6d16c22ace752
dudosyka <45633581+dudosyka@users.noreply.github.com>
2021-09-05 15:08:26 +0300

    Password updater

---
+++ graphql/pass.js
@@ -0,0 +1,310 @@
+module.exports = [
+ {
+   "id": 9001,
+   "password": "D62unmz4"
+ },
+ {
+   "id": 9002,
+   "password": "FCwsoARM"
+ },
+ {
+   "id": 9003,
+   "password": "mCQJNXA2"
+ },
+ {
+   "id": 9004,
+   "password": "OrZimHrs"
+ },
+ {
+   "id": 9005,
+   "password": "sitAJOOj"
+ },
+ {
+   "id": 9006,
+   "password": "vGjOFXAz"
+ },
+ {
+   "id": 9007,
+   "password": "ME8kfkSn"
+ },
+ {
+   "id": 9008,
+   "password": "3HqJebJj"
+ },
+ {
+   "id": 9009,
+   "password": "ISHVpMsm"
+ },
+ {
+   "id": 9010,
+   "password": "u1SozEQ7"
+ },
+ {
+   "id": 9011,
+   "password": "fOSlOMd7"
+ },
+ {
+   "id": 9012,
+   "password": "h8AIJajM"
+ },
+ {
+   "id": 9013,
+   "password": "Aoe0Y7zX"
+ },
+ {
+   "id": 9014,
+   "password": "h9rHZT4L"
+ },
+ {
+   "id": 9015,
+   "password": "r7tO89Lm"
+ },
+ {
+   "id": 9016,
+   "password": "zCFuZ3N0"
+ },
+ {
+   "id": 9017,
+   "password": "1HHo1gWk"
+ },
+ {
+   "id": 9018,
+   "password": "5N112FZt"
+ },
+ {
+   "id": 9019,
+   "password": "AN3AylY6"
+ },
+ {
+   "id": 9020,
+   "password": "fhpPqfEu"
+ },
+ {
+   "id": 9021,
+   "password": "86g8otp0"
+ },
+ {
+   "id": 9022,
+   "password": "BXhi6BwX"
+ },
+ {
+   "id": 9023,
+   "password": "SWEqkzCA"
+ },
+ {
+   "id": 9024,
+   "password": "NPPjaYV7"
+ },
+ {
+   "id": 9025,
+   "password": "pWeW3ZzQ"
+ },
+ {
+   "id": 9026,
+   "password": "IYtCCCFN"
+ },
+ {
+   "id": 9027,
+   "password": "YJ07JgrT"
+ },
+ {
+   "id": 9028,
+   "password": "e1ot9MJc"
+ },
+ {
+   "id": 9029,
+   "password": "MAPDWWvC"
+ },
+ {
+   "id": 9030,
+   "password": "C5HulQgr"
+ },
+ {
+   "id": 9031,
+   "password": "DJ2oD3HH"
+ },
+ {
+   "id": 9032,
+   "password": "5FSc5nw5"
+ },
+ {
+   "id": 9033,
+   "password": "pLrAgPT5"
+ },
+ {
+   "id": 9034,
+   "password": "wJLnVVN3"
+ },
+ {
+   "id": 9035,
+   "password": "2uAz3PKn"
+ },
+ {
+   "id": 9036,
+   "password": "Ck4xhORm"
+ },
+ {
+   "id": 9037,
+   "password": "Zm3WSIRj"
+ },
+ {
+   "id": 9038,
+   "password": "LEED7Hys"
+ },
+ {
+   "id": 9039,
+   "password": "kKryFMWL"
+ },
+ {
+   "id": 9040,
+   "password": "KRb0WNZJ"
+ },
+ {
+   "id": 9041,
+   "password": "ds30ZQTh"
+ },
+ {
+   "id": 9042,
+   "password": "Idnp6DMe"
+ },
+ {
+   "id": 9043,
+   "password": "s2z6GV77"
+ },
+ {
+   "id": 9044,
+   "password": "uNDy109u"
+ },
+ {
+   "id": 9045,
+   "password": "DL6ico2f"
+ },
+ {
+   "id": 9046,
+   "password": "bvPwvHgm"
+ },
+ {
+   "id": 9047,
+   "password": "6v2WKeJS"
+ },
+ {
+   "id": 9048,
+   "password": "bF7QKGmh"
+ },
+ {
+   "id": 9049,
+   "password": "LFzFnBKJ"
+ },
+ {
+   "id": 9050,
+   "password": "hiN0Fg47"
+ },
+ {
+   "id": 9051,
+   "password": "X9hW2Ydc"
+ },
+ {
+   "id": 9052,
+   "password": "ZamOwKkr"
+ },
+ {
+   "id": 9053,
+   "password": "1ogE1Kjl"
+ },
+ {
+   "id": 9054,
+   "password": "LfkJrrSW"
+ },
+ {
+   "id": 9055,
+   "password": "QoYcM4kq"
+ },
+ {
+   "id": 9056,
+   "password": "ocZ4wJjX"
+ },
+ {
+   "id": 9057,
+   "password": "0RKQ00At"
+ },
+ {
+   "id": 9058,
+   "password": "f0mi5IHo"
+ },
+ {
+   "id": 9059,
+   "password": "Nl4ULpdD"
+ },
+ {
+   "id": 9060,
+   "password": "sgpbcWLN"
+ },
+ {
+   "id": 9061,
+   "password": "xMnAlv8R"
+ },
+ {
+   "id": 9062,
+   "password": "GNFHSD0m"
+ },
+ {
+   "id": 9063,
+   "password": "B4MqEhgb"
+ },
+ {
+   "id": 9064,
+   "password": "rDNOfGQO"
+ },
+ {
+   "id": 9065,
+   "password": "aXmofdSF"
+ },
+ {
+   "id": 9066,
+   "password": "LXMU5xpE"
+ },
+ {
+   "id": 9067,
+   "password": "Gh8YSlQL"
+ },
+ {
+   "id": 9068,
+   "password": "vKJGuZMq"
+ },
+ {
+   "id": 9069,
+   "password": "2liJrpjn"
+ },
+ {
+   "id": 9070,
+   "password": "ziWKJPf6"
+ },
+ {
+   "id": 9071,
+   "password": "C4G4ZPN1"
+ },
+ {
+   "id": 9072,
+   "password": "LeUAqGMK"
+ },
+ {
+   "id": 9073,
+   "password": "052egyPr"
+ },
+ {
+   "id": 9074,
+   "password": "MtUruuu6"
+ },
+ {
+   "id": 9075,
+   "password": "fAbnpR45"
+ },
+ {
+   "id": 9076,
+   "password": "b6b0qGpU"
+ },
+ {
+   "id": 9077,
+   "password": "7bKDbiFA"
+ }
+]
